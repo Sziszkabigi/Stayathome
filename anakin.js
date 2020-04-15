06720938eb00e9cef5c3b7f54dc99369ed3b97e1
Brigitta Szikszai <sziszka0001@gmail.com>
2020-04-15 19:10:34 +0100

    added JS file

---
+++ anakin.js
@@ -0,0 +1,20 @@
+let colors = ["orange", "blue", "red"];
+
+console.log(colors);
+
+let post = {
+  title: "My post";
+  description: "My first post";
+  likeCount: 12;
+  comments: ["cool, nice one", "yeah"]
+};
+
+console.log(post);
+
+let age = 33;
+if (age >= 18){
+  console.log("you are an adult");
+  else
+    console.log("You are a child");
+}
+console.log("This is the enf code");
